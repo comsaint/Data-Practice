1121443bef901fc6c9ed9f7d3ac60a0885189753
comsaint <comsaint26@hotmail.com>
2018-10-16 17:56:51 +0800

    add dropping bounced users and split single session/returning users

--- ga_revenue_predict/main.py
+++ ga_revenue_predict/main.py
@@ -1,8 +1,16 @@
 import logging
 
 if __name__ == '__main__':
-    from .pipelines import preprocessing, read_parsed_data
+    logging.getLogger().setLevel(logging.INFO)
+
+    from .pipelines import preprocessing, read_parsed_data, drop_bounce_clients, split_by_returning
     #preprocessing()
     df_train, df_test = read_parsed_data()
-    print(df_train.head())
-    print(df_train.dtypes)
+    df_train, df_test, df_bounce = drop_bounce_clients(df_train, df_test)
+    df_single_train, df_single_test, df_return_train, df_return_test = split_by_returning(df_train, df_test)
+
+    df_single_train.to_csv('train_single_session.csv', index=False)
+    df_single_test.to_csv('test_single_session.csv', index=False)
+    #df_bounce.to_csv('bounce.csv', index=False)
+    #print(df_train.head())
+    #print(df_train.dtypes)
--- ga_revenue_predict/pipelines.py
+++ ga_revenue_predict/pipelines.py
@@ -2,6 +2,7 @@
 Pipelines and transforms to be performed.
 """
 import logging
+import pandas as pd
 from .Loader import Loader
 from .Settings import USE_COLS, NUM_COLS, TARGET
 from .CommonFunctions import store_df_and_dtypes, read_df_and_dtypes, deduplicate_repeated_sessionId
@@ -10,7 +11,7 @@ from .sk_util import RemoveColumns
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import FunctionTransformer
 from .Paths import FULLPATH_DATA_TRAIN_PARSED, FULLPATH_DATA_TEST_PARSED
-
+import datetime
 
 def preprocessing():
     # Preprocessing steps, dropping unuse columns, fix dtypes, merge duplicated session IDs, impute missing values.
@@ -59,13 +60,11 @@ def preprocessing():
                                               'totals.hits': int,
                                               'totals.newVisits': int,
                                               'totals.pageviews': int,
-                                              'totals.transactionRevenue': float,
-                                              'trafficSource.isTrueDirect': bool})
+                                              'totals.transactionRevenue': float})
     df_test_parsed = df_test_parsed.astype({'totals.bounces': int,
                                             'totals.hits': int,
                                             'totals.newVisits': int,
-                                            'totals.pageviews': int,
-                                            'trafficSource.isTrueDirect': bool})
+                                            'totals.pageviews': int})
 
     print(df_train_parsed.head(10))
     print(df_train_parsed.dtypes)
@@ -79,3 +78,53 @@ def read_parsed_data():
     df_train = read_df_and_dtypes(path=FULLPATH_DATA_TRAIN_PARSED)
     df_test = read_df_and_dtypes(path=FULLPATH_DATA_TEST_PARSED)
     return df_train, df_test
+
+
+def drop_bounce_clients(df_train, df_test):
+    """
+    Discard all clients without any non-bounce session.
+    :param df_train:
+    :param df_test:
+    :return:
+    """
+    df = pd.concat([df_train, df_test], axis=0, sort=True)  # concat train and test set
+    logging.info("Total number of unique clients: {}".format(df['fullVisitorId'].nunique()))
+    df_lite = df[['fullVisitorId', 'sessionId', 'totals.bounces']]
+    # count no. of sessions and no. of bounces
+    df_grp = df_lite.groupby('fullVisitorId').agg({'sessionId': 'count', 'totals.bounces': 'sum'}).reset_index()
+    lst_bounce_clients = df_grp[df_grp['sessionId'] == df_grp['totals.bounces']]['fullVisitorId'].tolist()
+    logging.info("{} bounced users found, discard.".format(len(lst_bounce_clients)))
+    df_no_bounce = df[~df['fullVisitorId'].isin(lst_bounce_clients)]
+    df_bounce = df[df['fullVisitorId'].isin(lst_bounce_clients)]
+    # Split back to train and test set
+    df_train_out = df_no_bounce[df_no_bounce['date'].dt.date <= datetime.date(2017, 8, 1)]
+    df_test_out = df_no_bounce[df_no_bounce['date'].dt.date > datetime.date(2017, 8, 1)]\
+        .drop('totals.transactionRevenue', axis=1)
+    return df_train_out, df_test_out, df_bounce
+
+
+def split_by_returning(df_train, df_test):
+    """
+    Split out clients by total number of sessions (single or >=2)
+    :param df_train:
+    :param df_test:
+    :return:
+    """
+    df = pd.concat([df_train, df_test], axis=0, sort=True)
+    df_grp = df.groupby('fullVisitorId').agg({'sessionId': 'count'}).reset_index()
+    lst_single = df_grp[df_grp['sessionId'] > 1]['fullVisitorId'].unique().tolist()
+    df_single = df[df['fullVisitorId'].isin(lst_single)]
+    df_return = df[~df['fullVisitorId'].isin(lst_single)]
+    logging.info("Total # of clients with only 1 session: {}".format(df_single['fullVisitorId'].nunique()))
+    logging.info("Total # of clients with more than 1 session: {}".format(df_return['fullVisitorId'].nunique()))
+    df_single_train = df_single[df_single['date'].dt.date <= datetime.date(2017, 8, 1)]
+    df_single_test = df_single[df_single['date'].dt.date > datetime.date(2017, 8, 1)]\
+        .drop('totals.transactionRevenue', axis=1)
+    df_return_train = df_return[df_return['date'].dt.date <= datetime.date(2017, 8, 1)]
+    df_return_test = df_return[df_return['date'].dt.date > datetime.date(2017, 8, 1)]\
+        .drop('totals.transactionRevenue', axis=1)
+    logging.info("Split of train/test 1 session clients: {}::{}"
+                 .format(df_single_train['fullVisitorId'].nunique(), df_single_test['fullVisitorId'].nunique()))
+    logging.info("Split of train/test returning clients: {}::{}"
+                 .format(df_return_train['fullVisitorId'].nunique(), df_return_test['fullVisitorId'].nunique()))
+    return df_single_train, df_single_test, df_return_train, df_return_test
